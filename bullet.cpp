90089f07f981d69956b2bc3833ff203e69b989d5
Jimmy3601 <1366803225@qq.com>
2023-07-03 22:28:25 +0800

    完成子弹功能（碰撞消失造成伤害）
    碰撞后消失并造成伤害（不会对发射的玩家造成伤害）

--- bullet.cpp
+++ bullet.cpp
@@ -1,8 +1,32 @@
 
 #include "bullet.h"
 
-Bullet::Bullet()
-{
+Bullet::Bullet(int x, int y, double r, int id_, const QPixmap *pixmap_, QGraphicsScene *scene_, double v_, int a_, int d, Player *p):
+    Object(x, y, r, v_, id_, pixmap_, scene_, v_, a_), parent(p), damage(d) {
+};
+
+Bullet::~Bullet(){
 
 }
 
+void Bullet::object_update(QVector<Object*> & exo) {
+    qreal dx = v*cos(angle*3.1415/180), dy = -v*sin(angle*3.1415/180); //dy is -ve cuz +y in qt is in downward direction
+    qreal nx = pos().rx() + dx, ny = pos().ry() + dy;
+    if (nx>= 0 && ny >= 0 && nx <= 1000-2*radius && ny <= 800 - 2* radius) {//check if inside range
+        moveBy(dx, dy);
+        update();
+    }
+    else {
+        //qDebug() << "de";
+        is_deleted = 1;
+    }
+
+}
+
+void Bullet::player_collide(Player *target) {
+    if (parent != target) {
+        target->on_hurt(damage);
+        qDebug() << "shot!";
+        is_deleted = 1;
+    }
+};
--- bullet.h
+++ bullet.h
@@ -2,16 +2,20 @@
 #ifndef BULLET_H
 #define BULLET_H
 
-#include "object.h"
+#include "player.h"
 
 
-class Bullet//: public Object
+class Bullet: public Object
 {
     int damage;
 public:
-    Bullet();
+    Bullet(int x, int y, double r, int id_, const QPixmap *pixmap_, QGraphicsScene *scene_, double v_, int a_, int d, Player *p);
+    ~Bullet();
     Player *parent;
 
+
+    virtual void object_update(QList<Object*> & exo);
+    virtual void player_collide(Player *target);
 };
 
 #endif // BULLET_H
