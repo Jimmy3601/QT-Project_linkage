0f3fb2a9c5257c78b99971785c48cf43f3818ed6
Jimmy3601 <1366803225@qq.com>
2023-07-03 22:29:01 +0800

    引入血量，掉血机制

--- player.cpp
+++ player.cpp
@@ -1,43 +1,57 @@
 
 #include "player.h"
+#include <QTimer>
 
 const int SPACING = 20; //cuz the image files contain some empty space
 
-Player::Player(int x, int y, int r,int id_, const QPixmap *pixmap_, QGraphicsScene *scene_, int initial_angle): Object(x,y,r,3, id_, pixmap_, scene_, 0, initial_angle), health(10), health_max(10)
+Player::Player(int x, int y, int r,int id_, const QPixmap *pixmap_, QGraphicsScene *scene_, int initial_angle):
+    Object(x,y,r,3, id_, pixmap_, scene_, 0, initial_angle), health(10), health_max(10), is_hurt(0)
 {
-
+    shoot_interval = 1000;
 }
 
 void Player::shoot() {
 
 }
-
-void Player::object_update(const QVector<Object*> & exo) {
+void Player::object_update(QList<Object*> & exo) {
     bool collision = 0;
     int si = exo.size();
     qreal dx = v*cos(angle*3.1415/180), dy = -v*sin(angle*3.1415/180); //dy is -ve cuz +y in qt is in downward direction
-    qreal nx = pos().rx() + dx, ny = pos().ry() + dy;
+    qreal nx = get_centre().rx() + dx, ny = get_centre().ry() + dy;
     for (int i = 0; i !=si; ++i) {
         Object *other = exo[i];
         if (other!=this) {
             if (other->is_circle) {
-                qreal x2 = other->pos().rx(), y2 = other->pos().ry();
-                if (this->radius+this->radius-SPACING >=  sqrt((nx-x2)*(nx-x2)+(ny-y2)*(ny-y2))) collision = 1;
+                qreal x2 = other->get_centre().rx(), y2 = other->get_centre().ry();
+                if (0.8*this->radius+0.8*other->radius >=  sqrt((nx-x2)*(nx-x2)+(ny-y2)*(ny-y2))) {
+                    collision = 1;
+                    other->player_collide(this);
+                }
                 //qDebug() << "collision:" << self->get_centre();
             }
 
         }
 
     }
-    if (nx>= 0 && ny >= 0 && nx <= 1000-2*radius && ny <= 800 - 2* radius && !collision) //check if inside range
+    if (nx>= radius && ny >= radius && nx <= 1000-radius && ny <= 800 - radius && !collision) //check if inside range
         moveBy(dx, dy);
     this->setRotation((450-angle)%360);
     update();
 }
 
 void Player::on_hurt(int damage) {
-
+    health -= damage;
+    setOpacity(0.2);
+    is_hurt = 1;
+    qDebug() << health;
+    if (health <= 0) {
+        qDebug() << "player"<<id+1<< "died!";
+        is_deleted = 1;
+    }
 }
 
+void Player::use_skill() {
+
+}
 
 
--- player.h
+++ player.h
@@ -8,15 +8,43 @@ class Bullet;
 
 class Player: public Object
 {
-
 public:
     Player(int x, int y, int r, int id_, const QPixmap *pixmap_, QGraphicsScene *scene_, int initial_angle);
     //~Player();
     int health, health_max;
+    int shoot_interval; // in ms
+    bool is_hurt;
+    int buff, buff_duration; //in ms
+    /*buff list(id)
+     * 0: none
+     * 1: gain 100%/400% extra movement speed
+     * 2: gain 100%/400% extra bullet speed
+     * 3: gain 100%/400% extra rotation speed
+     * 4: bullet deal 100%/300% extra damage
+     * 5: shoot interval beecome 0.5/0.2 s
+     * instant effect: heal 1/5 health
+     * */
 
     virtual void shoot();
-    virtual void object_update(const QVector<Object*> & exo);
+    virtual void object_update(QList<Object*> & exo);
     virtual void on_hurt(int damage);
+    virtual void use_skill();
+};
+
+class Eecs{
+
+};
+
+class Guanghua {
+
+};
+
+class Yuanpei{
+
+};
+
+class Xinya{
+
 };
 
 #endif // PLAYER_H
